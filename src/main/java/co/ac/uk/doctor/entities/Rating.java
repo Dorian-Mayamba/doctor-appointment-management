66d6e08682e7c3fc832ece55e4a84e72fb13b8d5
Dorian Mayamba <dorian.mayamba@gmail.com>
2024-04-16 02:07:19 +0100

    fixed entity annotation

--- src/main/java/co/ac/uk/doctor/entities/Rating.java
+++ src/main/java/co/ac/uk/doctor/entities/Rating.java
@@ -1,9 +1,6 @@
 package co.ac.uk.doctor.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.*;
 
 @NoArgsConstructor
@@ -11,6 +8,7 @@ import lombok.*;
 @Getter
 @Setter
 @Builder
+@Entity
 public class Rating {
     @Id
     private Long id;
